4dd122ef83db70d152790e31c4c94a26919c4298
Sawandi Wilson <sawandiwilson@gmail.com>
2020-07-08 16:51:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,6 +1,6 @@
 # require modules here
 
-def load_library(yml_file)
+def load_library(yaml_file)
   #This method should return a hash where each key is the name of an emoticon. 
   response = {"get_meaning" => {}, "get_japanese_emoticon" => {}}
   #Each emoticon name should point to a nested hash containing two keys, :english and :japanese. These keys will point to English and Japanese versions of the emoticon. 
