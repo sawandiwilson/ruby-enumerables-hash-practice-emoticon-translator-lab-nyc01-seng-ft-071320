d0f568373eb2b7acd08785269d921622a78cf203
Sawandi Wilson <sawandiwilson@gmail.com>
2020-07-08 16:53:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,13 @@
 # require modules here
 
 def load_library(emoticon_file)
- 
+ emoticons = YAML.load_file(file_path)
+  new_hash = {}
+
+  emoticons.each do |key,value|
+    new_hash[key] = {}
+    new_hash[key][:english] = value[0]
+    new_hash[key][:japanese] = value[1]
 end
 
 def get_japanese_emoticon
