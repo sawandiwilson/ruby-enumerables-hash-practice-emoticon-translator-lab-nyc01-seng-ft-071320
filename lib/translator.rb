bca62382e8dc8af8a70f52e66704cc9fdacda8db
Sawandi Wilson <sawandiwilson@gmail.com>
2020-07-08 16:46:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,6 +3,7 @@
 def load_library
   # code goes here
   #Write a method, load_library, that loads the emoticons.yml file. 
+  .each
   #This method should return a hash where each key is the name of an emoticon. 
   
   #Each emoticon name should point to a nested hash containing two keys, :english and :japanese. These keys will point to English and Japanese versions of the emoticon. 
